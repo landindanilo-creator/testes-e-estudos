10b5782610c66badc6c2a06476bcc51b5c692e96
landindanilo-creator <landindanilo@gmail.com>
2026-04-22 20:35:54 -0300

    alterações feitas por @landindanilo

--- teste7.py
+++ teste7.py
@@ -1,41 +1,52 @@
-cadastro_nome = []
+cadastro_clientes = []
 cadastro_cpf = []
 cadastro_telefone = []
-cadastro_celular = {}
+cadastro_celular = []
 cadastro_endereco = []
 cadastro_cep = []
 
-def cadastro_cliente(cadastro_nome, cadastro_cpf, cadastro_telefone, cadastro_celular, cadastro_endereco):
-    cadastro_nome ==input("Digite seu nome completo: ")
-    cadastro_cpf == int(input("Digite seu CPF: "))
-    cadastro_telefone = int(input("Digite seu telefone"))
-    cadastro_celular = int(input("Digite seu celular: "))
+#Função para cadastro
+def cadastro_cliente(nome, cpf, telefone, celular, endereco):
+    cadastro_clientes.append(nome)
+    cadastro_cpf.append(cpf)
+    cadastro_telefone.append(telefone)
+    cadastro_celular.append(celular)
+    cadastro_endereco.append(endereco)
+    
+    nome = input("Digite seu nome completo: ")
+    cpf = int(input("Digite seu CPF: "))
+    telefone = int(input("Digite seu telefone: "))
+    celular = int(input("Digite seu celular: "))
     endereco = input("Digite seu endereço: ")
 
-while True:
-    print("Bem Vindo ao sistema de cadastro de clientes!")
-    print("1 - Cadastrar Cliente")
-    print("2 - Lista de Clientes")
-    print("3 - Sair")
-    opc = int(input("Escolha uma opção!"))
+#Lista de clientes
+def lista_clientes():
+    print("Lista de clientes cadastrados:")
+    for i in range(len(cadastro_clientes)):
+        print(f"Nome: {cadastro_clientes[i]}\n, CPF: {cadastro_cpf[i]}\n, Telefone: {cadastro_telefone[i]}\n, Celular: {cadastro_celular[i]}\n, Endereço: {cadastro_endereco[i]}")
+
+#Menu de opções
+print("Bem Vindo ao sistema de cadastro de clientes!")
+print("1 - Cadastrar Cliente")
+print("2 - Lista de Clientes")
+print("3 - Sair")
+opc = int(input("Escolha uma opção: "))
+
+while opc != 3:
+   
     
+    #Menu de opções
     if opc == 1:
-        cadastro_cliente(cadastro_nome, cadastro_cpf, cadastro_telefone, cadastro_celular, cadastro_endereco)
-        print("Cadastro bem sucedido!")
+        cadastro_cliente("", 0, 0, 0, "")
+
     elif opc == 2:
-        cadastro_cliente(cadastro_nome, cadastro_cpf, cadastro_telefone, cadastro_celular, cadastro_endereco)
-        print("Lista de Cadastro de clientes!")
-        for i in range(len(cadastro_nome[i])):
-            print(f"Nome: {cadastro_nome[i]}")
-        for i in range(len(cadastro_cpf[i])):
-            print(f"CPF: {cadastro_cpf[i]}")
-        for i in range(len(cadastro_telefone[i])):
-            print(f"Telefone: {cadastro_telefone[i]}")
-        for i in range(len(cadastro_celular[i])):
-            print(f"Celular: {cadastro_celular[i]}")
-        for i in range(len(cadastro_endereco[i])):
-            print(f"Endereço: {cadastro_endereco[i]}")
+        lista_clientes()
 
     elif opc == 3:
-        print("Encerrando sistema!")
+        print("Encerrando o programa!")
         break
+    else:
+        print("Opção invalida! Escolha outra opção.")
+
+    
+
